2f44fba3406907897b607e6ba105fbdc4c85cf47
mouhamamd-kl <mouhammadk44@gmail.com>
2025-05-24 15:41:42 +0300

    .

---
+++ app/Events/LocationUpdated.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class LocationUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param array $data
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * The channel the event should broadcast on.
+     *
+     * @return Channel
+     */
+    public function broadcastOn()
+    {
+        return new Channel('test-channel');
+    }
+
+    /**
+     * The event's broadcast name.
+     *
+     * @return string
+     */
+    public function broadcastAs()
+    {
+        return 'test-event';
+    }
+
+    /**
+     * Get the data to broadcast.
+     *
+     * @return array
+     */
+    public function broadcastWith(): array
+    {
+        return $this->data;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,39 @@
 <?php
 
+use App\Events\TestEvent;
+use App\Events\TestNotification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/test', function (Request $request) {
+    return 'hello';
+});
+Route::get('/trigger-event', function () {
+    event(new TestNotification([
+        'driver_id' => 1,
+        'lat' => 2,
+        'lng' => 2
+    ]));
+
+    return response()->json(['message' => 'Event triggered']);
+});
+Route::post('/trigger-event2', function (Request $request) {
+    event(new TestEvent([
+        'driver_id' => $request->driver_id,
+        'lat' => $request->lat,
+        'lng' => $request->lng
+    ]));
+
+    return response()->json([
+        'driver_id' => $request->driver_id,
+        'lat' => $request->lat,
+        'lng' => $request->lng
+    ]);
+});
