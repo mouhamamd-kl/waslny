930afe479684f7ccb8e342eebfed8f280941ef85
mouhamamd-kl <mouhammadk44@gmail.com>
2025-08-05 18:28:06 +0300

    feat(security): implement account suspension system
    This commit introduces a system for suspending user accounts. It includes a dedicated service for handling suspension logic and new request classes for creating and searching suspensions. This feature enhances the platform's ability to manage user accounts and enforce community guidelines.

---
+++ app/Services/SuspensionService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use App\Helpers\CacheHelper;
+use App\Models\Suspension;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class SuspensionService extends BaseService
+{
+    public function __construct(CacheHelper $cache)
+    {
+        parent::__construct(new Suspension, $cache);
+    }
+
+    public function searchSuspension(
+        array $filters = [],
+        int $perPage = 10
+    ): LengthAwarePaginator {
+        return $this->toggleCache(config('app.enable_caching'))
+            ->paginatedList(
+                filters: $filters,
+                relations: $this->relations,
+                perPage: $perPage,
+                columns: ['*'],
+                withCount: []
+            );
+    }
+}
