2a028f0c5b939094cefe31dd7419608d82e6432b
mouhamamd-kl <mouhammadk44@gmail.com>
2025-08-05 18:36:25 +0300

    feat(drivers): add jobs for driver search and proximity check
    This commit introduces two new jobs to handle driver-related tasks asynchronously. The FindDriverForTrip job is responsible for finding a suitable driver for a trip, while the CheckDriverProximity job checks the driver's proximity to the pickup location. These jobs improve the scalability and performance of the driver assignment process.

---
+++ app/Jobs/FindDriverForTrip.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Events\DriverAssigned;
+use App\Events\DriverPreAssigned;
+use App\Enums\TripStatusEnum;
+use App\Enums\TripTimeTypeEnum;
+use App\Events\SearchTimeout;
+use App\Models\Trip;
+use App\Services\TripService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class FindDriverForTrip implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $trip;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Trip $trip)
+    {
+        $this->trip = $trip;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(TripService $tripService)
+    {
+        // trip_flow
+        if ($this->trip->driver_id) {
+            return;
+        }
+
+        if ($this->trip->search_expires_at->isPast()) {
+            $tripService->update($this->trip->id, ['trip_status_id' => TripStatusEnum::SystemCancelled->value]);
+            event(new SearchTimeout($this->trip));
+            return;
+        }
+
+        $driver = $tripService->findAndAssignDriver($this->trip);
+
+        if ($driver) {
+            if ($this->trip->trip_time_type_id === TripTimeTypeEnum::INSTANT->value) {
+                event(new DriverAssigned($this->trip, $driver));
+            } else {
+                event(new DriverPreAssigned($this->trip, $driver));
+            }
+        } else {
+            self::dispatch($this->trip)->delay(now()->addSeconds(15));
+        }
+    }
+}
